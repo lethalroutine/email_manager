684b4546933e4a0e3178f9ccf4c5c4633da51f79
agajda <aleksander.gajda1@gmail.com>
2020-04-18 15:24:10 +0200

    Add models for email

--- coreapp/models.py
+++ coreapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipient(models.Model):
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    email_address = models.EmailField()
+
+
+class Email(models.Model):
+    recipients = models.ForeignKey('Recipient')
+    sender = models.EmailField()
+    subject = models.CharField(max_length=50)
+    body = models.TextField()
+    status = models.CharField(choices=[('P', 'pending'), ('S', 'sent')])
