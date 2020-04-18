cdc85d5cf00450f74c7299d925754b5e84580017
agajda <aleksander.gajda1@gmail.com>
2020-04-18 17:58:56 +0200

    Add simple tests for models

--- coreapp/models.py
+++ coreapp/models.py
@@ -4,12 +4,12 @@ from django.db import models
 class Recipient(models.Model):
     first_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
-    email_address = models.EmailField()
+    email_address = models.EmailField(max_length=50)
+    parent_email = models.ForeignKey('Email', on_delete=models.CASCADE)
 
 
 class Email(models.Model):
-    recipients = models.ForeignKey('Recipient')
-    sender = models.EmailField()
+    sender = models.EmailField(max_length=50)
     subject = models.CharField(max_length=50)
     body = models.TextField()
-    status = models.CharField(choices=[('P', 'pending'), ('S', 'sent')])
+    status = models.CharField(choices=[('P', 'pending'), ('S', 'sent')], max_length=1)
--- coreapp/tests.py
+++ coreapp/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
+from coreapp import models
+
+
+def create_test_recipient(parent_email, email_address='test@test.com'):
+    return models.Recipient.objects.create(
+        first_name='',
+        last_name='',
+        parent_email=parent_email,
+        email_address=email_address
+    )
+
+
+class BasicModelsTestCase(TestCase):
+    def setUp(self):
+        self.test_email_data = {
+            'sender': 'test@test.com',
+            'subject': 'test subject',
+            'body': 'test body',
+            'status': 'pending',
+        }
+
+    def test_email_model_can_be_created(self):
+        email = models.Email.objects.create(**self.test_email_data)
+
+        self.assertEqual(email.subject, self.test_email_data['subject'])
+
+    def test_recipient_is_assigned_to_email(self):
+        email = models.Email.objects.create(**self.test_email_data)
+        create_test_recipient(email)
+
+        self.assertEqual(email.recipient_set.first().email_address, 'test@test.com')
+
+    def test_email_can_have_multiple_recipients(self):
+        email = models.Email.objects.create(
+            **self.test_email_data
+        )
+        create_test_recipient(email)
+        create_test_recipient(email)
+
+        self.assertEqual(len(email.recipient_set.all()), 2)
